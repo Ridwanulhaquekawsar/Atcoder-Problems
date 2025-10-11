122bd7562774c2fb79867cf5ea93444c72268776
Ridwanul Haque Kawsar <kawsarhaque430@gmail.com>
2025-10-12 00:30:43 +0600

    Update and rename Sum_of_digit_sequence.cpp to Sum_of_digits_sequence.cpp

--- Sum_of_digits_sequence.cpp
+++ Sum_of_digits_sequence.cpp
@@ -37,3 +37,42 @@ int main(){
     cin.tie(nullptr); solve();
     return 0;
 }
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int N; vector<int>arr;
+
+int digit_sum(int val){
+    int sum=0;
+    while(val>0){
+        sum+=(val%10);
+        val/=10;
+    }
+    
+    return sum;
+}
+
+void solve(){
+    cin>>N; arr.resize(N+1);
+    arr[0]=1; arr[1]=1;
+    
+    if(N>1){
+        int i=2;
+        while(i<=N){
+            arr[i]=arr[i-1]+digit_sum(arr[i-1]);
+            i++;
+        }
+    }
+    
+    cout<<arr[N];
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr); solve();
+    return 0;
+}
